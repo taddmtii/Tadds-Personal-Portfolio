fef406c00da22682b233c56901acb734adfc63ce
Tadd Trumbull II <taddtrumbullii@gmail.com>
2024-11-11 09:33:11 -0500

    small navbar ui changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,13 @@ export default function Navbar () {
     return (
         <>
             <div className='flex w-[90vw] h-20 rounded-3xl items-center justify-center bg-blue-400 bg-opacity-40'>
+                <div className="flex font-bold font-mono">
+                    <h1>Tadd Trumbull II</h1>
+                </div>
                 <ul className="flex space-x-10">
-                    <li className="hover:underline"><a href='/Home'>Home</a></li>
-                    <li className="hover:underline"><a href='/Projects'>Projects</a></li>
-                    <li className="hover:underline"><a href='/About'>About me</a></li>
+                    <li className="hover:underline font-bold font-mono"><a href='/Home'>Home</a></li>
+                    <li className="hover:underline font-bold font-mono"><a href='/Projects'>Projects</a></li>
+                    <li className="hover:underline font-bold font-mono"><a href='/About'>About me</a></li>
                 </ul>
             </div>
         </>
